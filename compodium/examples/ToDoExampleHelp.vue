6eace1864303511bfbc664a3488065039aa89338
sfxcode <tom@sfxcode.com>
2025-10-18 15:19:27 +0200

    feat: add ToDoExampleHelp and preview components

---
+++ compodium/examples/ToDoExampleHelp.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Todo from '@/components/Todo.vue'
+
+extendCompodiumMeta({
+  defaultProps: {
+    text: 'Help',
+    icon: 'i-mdi-help-circle-outline',
+  },
+})
+</script>
+
+<template>
+  <Todo>Some Help Text</Todo>
+</template>
---
+++ compodium/preview.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="workspace" class="mt-40">
+    <div class="card">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang='scss'>
+@use '../app/App.scss';
+
+#workspace {
+  padding-left: 0;
+  margin-right: 30px;
+}
+
+#workspace.collapsed {
+  padding-left: 0;
+}
+
+#workspace.onmobile {
+  padding-left: 0;
+}
+</style>
